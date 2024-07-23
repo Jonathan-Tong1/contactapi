5f356fed260c3b5c56caff50f37a766a06f6efd7
Jonathan Tong <jontong321@gmail.com>
2024-07-23 12:04:21 -0400

    Updated contact

---
+++ src/main/java/com/codewithjonathan/contactapi/domain/Contact.java
@@ -0,0 +1,12 @@
+package com.codewithjonathan.contactapi.domain;
+
+public class Contact {
+    private String id;
+    private String name;
+    private String email;
+    private String title;
+    private String phone;
+    private String address;
+    private String status;
+    private String photoUrl;
+}
