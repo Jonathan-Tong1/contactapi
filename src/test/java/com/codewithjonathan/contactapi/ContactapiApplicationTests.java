5e8d788c3261e4e4b7eb7300087a1f72d21fc4d5
Jonathan Tong <jontong321@gmail.com>
2024-07-23 12:04:26 -0400

    Updated contact

---
+++ src/test/java/com/codewithjonathan/contactapi/ContactapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithjonathan.contactapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
