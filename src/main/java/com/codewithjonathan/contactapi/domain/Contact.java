3db9b81993ca3318c004091206a24099bce3212d
Jonathan Tong <jontong321@gmail.com>
2024-07-23 12:08:45 -0400

    Updated contact

--- src/main/java/com/codewithjonathan/contactapi/domain/Contact.java
+++ src/main/java/com/codewithjonathan/contactapi/domain/Contact.java
@@ -1,6 +1,24 @@
 package com.codewithjonathan.contactapi.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+@Entity
+@Table(name = "contacts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class Contact {
+    @Id
+    @UuidGenerator
+    @Column(name = "id", unique = true, updatable = false)
     private String id;
     private String name;
     private String email;
